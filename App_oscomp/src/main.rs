872a1f38e76b5a238ea89ebef79c8436ba7b3ec0
ZreXoc <zeexoc@outlook.com>
2025-05-24 16:18:15 +0800

    fix

--- App_oscomp/src/main.rs
+++ App_oscomp/src/main.rs
@@ -4,84 +4,76 @@
 #![no_main]
 #![feature(stmt_expr_attributes)]
 
-use core::arch::global_asm;
-
-#[macro_use]
 extern crate axstd;
 #[macro_use]
 extern crate axlog;
 #[macro_use]
 extern crate alloc;
 
-use alloc::borrow::Cow;
+use alloc::string::String;
 use alloc::string::ToString;
 use alloc::sync::Arc;
-use axerrno::AxResult;
-use axfs::api::{create_dir, read_dir};
-use axhal::{
-    arch::{TrapFrame, UspaceContext},
-    mem::VirtAddr,
-    trap::{SYSCALL, register_trap_handler},
-};
-use axmm::{AddrSpace, kernel_aspace};
-use axmono::{loader::load_app_from_disk, mm::load_elf_to_mem};
-use axstd::println;
+use axhal::arch::UspaceContext;
+use axmono::{loader::load_elf_from_disk, mm::load_elf_to_mem};
 use axsync::Mutex;
 
 //#[cfg_attr(feature = "axstd", unsafe(no_mangle))]
 #[unsafe(no_mangle)]
 fn main() {
-    //axfs::mount("/dev/vda2", "/mnt", 1).unwrap();
-    //axfs::mount("/disk2.img", "/mnt", 1).unwrap();
+    //run_testcode("libctest", "musl");
     /*
-     *for entry in read_dir("/").unwrap(){
-     *    let entry = entry.unwrap();
-     *    println!("entry: {:?}", entry);
-     *}
+     *run_testcase(
+     *    "/riscv/musl/busybox",
+     *    "/riscv/musl",
+     *    Some(&[
+     *        "/riscv/musl/busybox".into(),
+     *        "ash".into(),
+     *        "/riscv/musl/libc-bench".into(),
+     *    ]),
+     *);
      */
-
-    //print!("hello world");
-    //axfs::mount("/disk2.img", "/mnt", 1);
-
-    // file_type=jsonc to enable IDE format and comment
-    let TESTCASES = include!("./testcase_list.jsonc");
-
-    /*
-     *    let read_dir = axfs::api::read_dir("/").unwrap();
-     *    for entry in read_dir {
-     *        let entry = entry.unwrap();
-     *        println!("entry: {:?}", entry);
-     *
-     *        if !entry.file_type().is_file() {
-     *            continue;
-     *        }
-     *        run_testcase(entry.path().as_str());
-     *    }
-     */
-    // run_testcase("/musl/busybox");
-    
-    for &t in TESTCASES.iter() {
-        let name = t.split_at(12).1;
-        println!("Testing {name}:");
-        run_testcase(t);
-    }
+    run_testcase(
+        "/riscv/musl/runtest.exe",
+        "/riscv/musl",
+        Some(&[
+            "runtest.exe".into(),
+            "-w".into(),
+            "entry-static.exe".into(),
+            "pthread_cancel_points".into(),
+        ]),
+    );
 }
 
-fn run_testcase(app_path: &str) -> isize {
-    let (entry_vaddr, user_stack_base, uspace) = load_elf_to_mem(
-        load_app_from_disk(app_path).unwrap(),
-        Some(&[app_path.into(),"ls".into()]),
-        None,
-    )
-    .unwrap();
+fn run_testcode(name: &str, c_lib: &str) {
+    let shell = "/riscv/musl/busybox";
+    let pwd = format!("/riscv/{}", c_lib);
+    let testcode = format!("/riscv/{}/{}_testcode.sh", c_lib, name);
+    run_testcase(
+        shell,
+        pwd.as_str(),
+        Some(&[shell.into(), "ash".into(), testcode.into()]),
+    );
+}
+
+fn run_testcase(app_path: &str, pwd: &str, args: Option<&[String]>) -> isize {
+    let (entry_vaddr, user_stack_base, tp, uspace) =
+        load_elf_to_mem(load_elf_from_disk(app_path).unwrap(), args, None).unwrap();
     debug!(
         "app_entry: {:?}, app_stack: {:?}, app_aspace: {:?}",
         entry_vaddr, user_stack_base, uspace,
     );
 
-    let uctx = UspaceContext::new(entry_vaddr.into(), user_stack_base, 2333);
-
-    let user_task = axmono::task::spawn_user_task(app_path, Arc::new(Mutex::new(uspace)), uctx);
+    let mut uctx = UspaceContext::new(entry_vaddr.into(), user_stack_base, 2333);
+    if let Some(tp) = tp {
+        uctx.set_tp(tp.as_usize());
+    }
+    let user_task = axmono::task::spawn_user_task(
+        app_path,
+        Arc::new(Mutex::new(uspace)),
+        uctx,
+        pwd.into(),
+        true,
+    );
 
     axtask::spawn_task_by_ref(user_task.clone());
 
